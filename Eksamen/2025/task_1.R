2f5afbc69c2d3e78107784820aaa5a79b224dbf4
Ragnhild Thielemann <ragnth@ulrik.uio.no>
2026-09-03 15:21:59 +0200

    woop

--- Eksamen/2025/task_1.R
+++ Eksamen/2025/task_1.R
@@ -4,7 +4,7 @@ library(tidyverse)
 data <- read_csv("esc.csv")
 
 
-View(data)
+
 
 ncol(data)
 sprintf("Det er %s land som deltar, og %s land som er i finalen",nrow(data),ncol(data)-4)
@@ -20,17 +20,21 @@ long <- pivot_longer(data,cols = (vektor),
                      values_to = "Score"
                      )
 
-View(long)
+
 
 #bytter ut de tomme verdiene med 0
 long <- long |> mutate(across(everything(), ~(replace_na(.x ,0))))
 
 #antall 12-ere
 #lager en ny tibble, der vi summerer sammen antall 12-ere for hvert tall
-antall_12 <- long |> summarise(s = sum(Score == 12), .by = contestant)
+sortert_land <- long |> summarise(s_12 = sum(Score == 12), .by = contestant)
 vektor_til_merging <- (long$Score[long$Awarding== "jury_score"])
 
+v = tibble(contestant =sortert_land$contestant ,Jury = vektor_til_merging)
+#merger de sammen
+sortert_land <- sortert_land |> left_join(v)
 
 
+ggplot(sortert_land) + geom_point(aes(x = s_12, y=Jury )) + labs ( x = "Antall 12-ere", y = "Score fra Juryen")
 
-print(antall_12)
+
